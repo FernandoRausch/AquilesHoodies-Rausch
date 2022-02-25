87581ef9c69693fc7dec9c2743a914b7142d28f8
Fernando Rausch <fernandorausch@gmail.com>
2022-02-24 23:23:50 -0300

    Desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const NavBar = () => {
+    return(
+        <header>
+            <h1>Aquiles Hoodies</h1>
+            <nav>
+                <a href="#">Inicio </a>
+                <a href="#">Shop </a>
+                <a href="#">Nosotros </a>
+                <a href="#">Contacto </a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
