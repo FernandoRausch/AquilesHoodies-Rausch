4930a50612f5f701e67b5346b6f8dc9f8fd378fe
Fernando Rausch <fernandorausch@gmail.com>
2022-03-03 23:49:49 -0300

    Desafio clase 5

---
+++ src/components/ItemCount.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+
+const ItemCount = (props) => {
+    let [estado, setEstado] = useState(0)
+    const handleSumar = () => {
+        if(estado<props.stock){
+            setEstado(estado+1)}
+        }
+
+    const handleRestar = () => {
+        if(estado>1)
+        setEstado(estado-1)}
+
+    const handleAdd = () => {
+        props.onAdd= props.onAdd + estado
+    }
+
+    
+    return(<>
+        <p>{estado}</p>
+        <button onClick={handleSumar}>+</button>
+        <button onClick={handleRestar}>-</button>
+        <button onClick={handleAdd}>Agregar al carrito</button>
+        </>
+    );
+}
+    
+
+export default ItemCount
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,6 +1,16 @@
+import React from "react"
+import ItemCount from "./ItemCount"
+
 const ItemListContainer = (props) => {
     return(
+        <>
         <div id="greeting">{props.greeting}</div>
+        <ItemCount
+            stock={7}
+            initial={1}
+            onAdd={0} 
+        ></ItemCount>
+        </>
     )
 }
 
